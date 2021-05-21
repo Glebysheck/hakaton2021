c4b2c3160cb16b983d72ace4eb3bdf5eba0853bd
sedri <sedringleb2@gmail.com>
2021-05-21 15:00:56 +0300

    Initial commit

---
+++ app/Models/Posters_list.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Posters_list extends Model
+{
+    use HasFactory;
+}
